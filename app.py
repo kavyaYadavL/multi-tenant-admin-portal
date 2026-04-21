d25ed27cc0bcff01d572ba0131397221d176a35e
Kavya <kavyalakshman09@gmail.com>
2026-04-21 20:08:31 +0530

    Day 1: Flask API with describe endpoint and Postman testing

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message": "AI Service Running"}
+
+@app.route("/health", methods=["GET"])
+def health():
+    return {
+        "status": "healthy",
+        "message": "AI Service is running"
+    }
+
+@app.route("/describe", methods=["POST"])
+def describe():
+    start_time = time.time()
+
+    try:
+        data = request.get_json()
+
+        if not data or "text" not in data:
+            return jsonify({
+                "status": "error",
+                "message": "Please provide 'text' in request body"
+            }), 400
+
+        text = data["text"]
+
+        # Basic analysis
+        words = text.split()
+        word_count = len(words)
+        char_count = len(text)
+
+        # Summary
+        summary = " ".join(words[:5])
+
+        # Sentiment
+        positive_words = ["good", "great", "happy", "excellent", "awesome", "nice", "love"]
+        negative_words = ["bad", "sad", "terrible", "worst", "poor", "hate"]
+
+        sentiment = "neutral"
+        for word in words:
+            if word.lower() in positive_words:
+                sentiment = "positive"
+                break
+            elif word.lower() in negative_words:
+                sentiment = "negative"
+                break
+
+        response_time = round((time.time() - start_time) * 1000, 2)
+
+        return jsonify({
+            "status": "success",
+            "input_text": text,
+            "analysis": {
+                "word_count": word_count,
+                "character_count": char_count,
+                "summary": summary,
+                "sentiment": sentiment
+            },
+            "meta": {
+                "response_time_ms": response_time
+            },
+            "message": "Text analyzed successfully"
+        })
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": "Something went wrong",
+            "error": str(e)
+        }), 500
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
