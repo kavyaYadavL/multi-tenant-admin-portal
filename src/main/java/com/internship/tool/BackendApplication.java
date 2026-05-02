62824c92162ebf46213e75e8088bcf8e22396ced
Anushaym625 <anushaym296@gmail.com>
2026-05-02 20:44:58 +0530

    Day 6 — Redis caching and RBAC implemented

--- src/main/java/com/internship/tool/BackendApplication.java
+++ src/main/java/com/internship/tool/BackendApplication.java
@@ -2,9 +2,11 @@ package com.internship.tool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @org.springframework.data.jpa.repository.config.EnableJpaAuditing
+@EnableCaching
 public class BackendApplication {
 
 	public static void main(String[] args) {
